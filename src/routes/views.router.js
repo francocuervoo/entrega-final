4d1066dcd07aeb877831619151b4f34d341583b4
francocuervoo <77750393+francocuervoo@users.noreply.github.com>
2021-12-17 10:38:22 -0300

    Update views.router.js

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import * as AuthMiddleware from "../middlewares/auth.middleware.js";
-import { cartView, loginView, signupView } from "../controllers/views.controller.js"
+import { cartView, loginView, signupView, failLoginView, failSignupView} from "../controllers/views.controller.js"
 
 const viewsRouter = Router();
 
 viewsRouter.get("/cart", AuthMiddleware.checkAuthentication, cartView)
 viewsRouter.get("/login", loginView)
 viewsRouter.get("/signup", signupView)
+viewsRouter.get("/failLogin", failLoginView)
+viewsRouter.get("/failSignup", failSignupView)
 
 
 export default viewsRouter;
